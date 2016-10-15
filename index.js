d788d889b541f1d38eb1f41cac08c173248806a5
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-10-15 22:49:08 +0700

    Added static file (react). And foreman as process manager for development

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 'use strict';
 
 const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const config = require(`./config`);
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.listen(config.http.port, () => {
+
+});
 
 module.exports = app;
