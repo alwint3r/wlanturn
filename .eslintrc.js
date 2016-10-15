882998da98c082caa10e7ac550128ca783e439e5
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-10-15 22:18:43 +0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "env": {
+      "node": true,
+    },
+    "rules": {
+      "quotes": [1, "backtick"],
+      "strict": 0,
+      "import/no-dynamic-require": 0,
+    },
+};
